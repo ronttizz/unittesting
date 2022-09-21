cbda5f5a2894fa1dddf76da0ad8937e197fe0ca4
ronttizz <toni.nissinen@gmail.com>
2022-09-21 10:06:56 +0300

    tests for get_total_price_of_products_by_type added

--- Nissinen_Toni_product/__tests__/productStorage.test.js
+++ Nissinen_Toni_product/__tests__/productStorage.test.js
@@ -8,3 +8,27 @@ describe("Test for constuctor", () => {
     expect(() => new ProductStorage()).toThrow("data storage missing");
   });
 });
+
+describe("Tests for get_total_price_of_products_by_type", () => {
+  const products = new ProductStorage(datastorage);
+
+  test("Test 1: get total cumulative price of toasters using default data", () => {
+    expect(products.get_total_price_of_products_by_type("toaster")).toEqual(315);
+  });
+
+  test("Test 2: get total cumulative price of phones using default data", () => {
+    expect(products.get_total_price_of_products_by_type("toaster")).toEqual(15);
+  });
+
+  test("Test 3: throw en exception `'nothing found with given'` using default data", () => {
+    expect(() => products.get_total_price_of_products_by_type("computer")).toThrow(
+      "nothing found with given"
+    );
+  });
+
+  test("Test 4: throw an exception `'missing parameter'`", () => {
+    expect(() => products.get_total_price_of_products_by_type()).toThrow(
+      "missing parameter"
+    );
+  });
+});
