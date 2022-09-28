a9371270c1fdc08e29d331502feb12da92c916b1
ronttizz <toni.nissinen@gmail.com>
2022-09-28 10:16:19 +0300

    find implementation

--- carProjectPromisesClass/carstorageClass.js
+++ carProjectPromisesClass/carstorageClass.js
@@ -8,6 +8,30 @@ module.exports = class Storage {
   }
 
   find(key, value) {
-    return new Promise(async (resolve, reject) => {});
-  }
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (key && value) {
+          if (!["model, license"].includes(key)) {
+            reject("key not found");
+          } else {
+            const data = await fs.readFile(this.storageFile, "utf8");
+            const cars = await JSON.parse(data);
+            // EXAMPLE oneliner
+            // const cars = await JSON.parse(await fs.readFile(this.storageFile,'utf8'));
+            const found = [];
+            for (let car of cars) {
+              if (car[key] == value) {
+                found.push(car);
+              }
+            }
+            resolve(found);
+          }
+        } else {
+          reject("parameter missing");
+        }
+      } catch (error) {
+        reject("read error");
+      }
+    }); // end of Promise
+  } // end of find
 };
