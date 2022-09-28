be4fae49828142f296ab1bf16dceab95b59be0a8
ronttizz <toni.nissinen@gmail.com>
2022-09-28 10:59:41 +0300

    few tests more

--- carProjectPromisesClass/__tests__/carstorageClass.test.js
+++ carProjectPromisesClass/__tests__/carstorageClass.test.js
@@ -10,4 +10,26 @@ describe("testing class with cars.json", () => {
 
     return expect(carStorage.find("licence", "ABC-1")).resolves.toEqual(expectedResult);
   });
+
+  test("test find with wrong key", () => {
+    return expect(carStorage.find("color", "blue")).rejects.toBe("key not found");
+  });
+
+  test("test missing parameter", () => {
+    return expect(carStorage.find("model")).rejects.toBe("parameter missing");
+  });
+
+  test("test model with Bored T-model", () => {
+    return expect(carStorage.find("model", "Bored T-model")).resolves.toEqual([
+      { model: "Bored T-model", licence: "ABC-1" },
+      { model: "Bored T-model", licence: "GTF-10" },
+    ]);
+  });
+});
+
+describe("wrong filename", () => {
+  test("testing with file x.json", () => {
+    const storage = new Storage("./x.json");
+    return expect(storage.find("licence", "ABC-1")).rejects.toBe("read error");
+  });
 });
