c0ae9f7a6d32b90d2df1fbc04557809f0f6a71fa
ronttizz <toni.nissinen@gmail.com>
2022-09-28 10:40:43 +0300

    typo fixed

--- carProjectPromisesClass/__tests__/carstorageClass.test.js
+++ carProjectPromisesClass/__tests__/carstorageClass.test.js
@@ -3,11 +3,11 @@
 const Storage = require("../carstorageClass");
 
 describe("testing class with cars.json", () => {
-  const carstorage = new Storage("./cars.json");
+  const carStorage = new Storage("./cars.json");
 
   test("test find with licence ABC-1", () => {
     const expectedResult = [{ model: "Bored T-model", licence: "ABC-1" }];
 
-    return expect(carstorage.find("licence", "ABC-1")).resolves.toEqual(expectedResult);
+    return expect(carStorage.find("licence", "ABC-1")).resolves.toEqual(expectedResult);
   });
 });
