0f7b324f678d52f40970a6cd1563d2056b0cd931
ronttizz <toni.nissinen@gmail.com>
2022-09-28 10:28:03 +0300

    demo for othercars

--- carProjectPromisesClass/demo.js
+++ carProjectPromisesClass/demo.js
@@ -7,3 +7,9 @@ const carStorage = new Storage("./cars.json");
 carStorage.find("model", "Nova").then(console.log).catch(console.log);
 
 carStorage.find("color", "Blue").then(console.log).catch(console.log);
+
+const carStorage2 = new Storage("./otherCars.json");
+
+carStorage2.find("model", "Nova").then(console.log).catch(console.log);
+
+carStorage2.find("color", "Blue").then(console.log).catch(console.log);
