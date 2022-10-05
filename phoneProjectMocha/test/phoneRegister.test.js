7240bea595956642e9b0fda7ec3cbf3335b89743
ronttizz <toni.nissinen@gmail.com>
2022-10-05 13:09:15 +0300

    getTypes tests

--- phoneProjectMocha/test/phoneRegister.test.js
+++ phoneProjectMocha/test/phoneRegister.test.js
@@ -4,3 +4,44 @@ const expect = require("chai").expect;
 
 const PhoneRegister = require("../phoneRegister");
 const phones = require("../phones.json");
+
+describe("test getTypes", function () {
+  it("using the default data", function () {
+    const register = new PhoneRegister(phones);
+    expect(register.getTypes()).to.be.deep.equal(["home", "work", "mobile"]);
+  });
+
+  it("test equal", function () {
+    const a = [1, 2, 3];
+    expect(a).to.deep.equal([1, 2, 3]);
+  });
+});
+
+describe("non-default data", function () {
+  it("persons have no phones", function () {
+    const testData = [
+      {
+        firstname: "Leila",
+        lastname: "Hökki",
+        phones: [],
+      },
+      {
+        firstname: "Matt",
+        lastname: "River",
+        phones: [],
+      },
+      {
+        firstname: "Vera",
+        lastname: "River",
+      },
+    ];
+
+    const register = new PhoneRegister(testData);
+    expect(register.getTypes()).to.deep.equal([]);
+  });
+
+  it("empty register", function () {
+    const register = new PhoneRegister([]);
+    expect(register.getTypes()).is.empty;
+  });
+});
